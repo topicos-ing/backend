6c1741caf2318e624de805ca16b2c59b7a90f408
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-10-20 10:21:23 -0300

    se crea proyecto

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/api', (req, res) => {res.json({"links": ["link1","link2", "link3"]})})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
