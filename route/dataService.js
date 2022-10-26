938bed8b50e4427b10bcfcaebda7316a2c407f05
tomasabreu <tomas.abreu@hotmail.com>
2022-10-26 00:07:51 -0300

    Language Filter & Code Cleanup

--- route/dataService.js
+++ route/dataService.js
@@ -1,42 +1,43 @@
-const Products = require('../models/product');
-const express = require('express');
-const router = express.Router(); 
+const Products = require("../models/product");
+const express = require("express");
+const router = express.Router();
 
 // Servicio para listar todos los docuentos
-router.get('/getdata',  (req, res) => {
-   Products.find()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+router.get("/", async (req, res) => {
+  try {
+    const data = await Products.find();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(400).json({ message: e });
+  }
 });
 
 // Endpoint para invocar por gtin y params opcionales
-router.get('/:gtin_code(\\d+)', async (req, res) => {
-    var gtin_code   = req.params.gtin_code;
-    console.log("Solicitud por el gtin: " + gtin_code);
-  
-    // Se agrega a la query de busqueda el gtin
-    var query = { gtin: gtin_code};
+router.get("/:gtin(\\d+)", async (req, res) => {
+  const { gtin } = req.params || {};
+  const { linkType, language } = req.query || {};
+  console.log("Solicitud por el gtin: " + gtin);
+  // Se agrega a la query de busqueda el gtin
+  let query = { gtin, linkType, language };
 
-    // console.log(JSON.stringify(req.query)); // TODO: DELETE -> TEST para ver todos los params opcionales 
-    // Parametros opcionales se agarran por req.query ejemplo de manejo
-    var linkTypeResult   = req.query.linkType; 
-    if (linkTypeResult) {
-        query.linkType =linkTypeResult;
-        console.log("Viajo en el request el parametro opcional linkType con el valor: " + linkTypeResult);
+  //FIXME var languageResult   = req.headers['accept-language'];
+  // console.log(languageResult)
+
+  // TODO: Delete. Para pruebas y para ver como queda la query antes del find
+  console.log("Query previa a ejecutar: " + JSON.stringify(query));
+  try {
+    const docs = await Products.find(query);
+    if (docs[0]) {
+      // Redireccion al sitio que tiene cargado el registro
+      console.log("Resultado de busqueda find: " + docs);
+      res.status(301).redirect(docs[0].uri);
+    } else {
+      res.status(404).json({ message: "Product Not found" });
     }
-    //FIXME var languageResult   = req.headers['accept-language']; 
-    // console.log(languageResult)
-
-    // TODO: Delete. Para pruebas y para ver como queda la query antes del find
-    console.log("Query previa a ejecutar: " + JSON.stringify(query))
-    Products.find(query, function (err, docs) {
-        if (err){ console.error.bind("Error obtenido al ejecutar la busqueda: ", err); }
-        else{
-           console.log("Resultado de busqueda find: " + docs);
-            // Redireccion al sitio que tiene cargado el registro
-           res.redirect(301, docs[0].uri);
-        }
-    });
+  } catch (error) {
+    console.error.bind("Error obtenido al ejecutar la busqueda: ", error);
+    res.status(400).json({ message: error });
+  }
 });
 
-module.exports = router; 
+module.exports = router;
