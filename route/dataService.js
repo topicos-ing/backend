5b8beca7340d50fe740866cf3d1547aabb9693f9
tomasabreu <42159630+tomasabreu@users.noreply.github.com>
2022-10-27 19:09:50 -0300

    Default link fix

--- route/dataService.js
+++ route/dataService.js
@@ -28,14 +28,22 @@ router.get("/:gtin(\\d+)", async (req, res) => {
   // TODO: Delete. Para pruebas y para ver como queda la query antes del find
   console.log("Query previa a ejecutar: " + JSON.stringify(query));
   try {
-    const docs = await Products.find(query);
+    let docs = await Products.find(query);
     if (docs[0]) {
       // Redireccion al sitio que tiene cargado el registro
       console.log("Resultado de busqueda find: " + docs);
       res.status(301).redirect(docs[0].uri);
-    } else {
-      res.status(404).json({ message: "Product Not found" });
+      return;
     }
+    docs = await Products.find({ gtin, linkType: "gs1:defaultLink" });
+    console.log("No hubo resutados redireccionando a default");
+    if (docs[0]) {
+      // Redireccion al sitio que tiene cargado el registro
+      console.log("Resultado de busqueda default find: " + docs);
+      res.status(301).redirect(docs[0].uri);
+      return;
+    }
+    res.status(404).json({ message: "Product Not found" });
   } catch (error) {
     console.error.bind("Error obtenido al ejecutar la busqueda: ", error);
     res.status(400).json({ message: error });
