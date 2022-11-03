0a44ca06171632be57067c2267ae20f28cba99fc
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-11-03 20:31:30 -0300

    tests productServices. test /products y /getin

---
+++ __tests__/productService.spec.js
@@ -0,0 +1,23 @@
+
+const baseURL = "https://resolver-backend-preprod.azurewebsites.net"
+//const app = require('../server.js')
+const request = require('supertest')
+
+describe('get all products /', () =>{
+  test('should respond 200 status code', async ()=>{
+    const response = await request(baseURL).get('/products').send()
+     expect(response.statusCode).toBe(200)
+     
+  })
+})
+
+describe('get product with getin 9506000134352',  () => {
+  test('should respond 302 status code', async () => {
+    const response = await request(baseURL).get('/9506000134352').send()
+    expect(response.statusCode).toBe(302)
+   
+  })
+})
+
+
+
