1727f0b85c2e2d76f483e173956b463c2f0806e9
ManuelaGarcia36 <manuela.garcia@mercadolibre.com>
2022-11-03 17:40:11 -0300

    added delete product and fix comments

--- route/productService.js
+++ route/productService.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Servicio para listar todos los docuentos
-router.get("/products", async (req, res) => {
+router.get("/products", (req, res) => {
   productSchema.find()
     .then(data =>  res.status(200).json(data))
     .catch(err => res.status(400).json({ message: err}));
@@ -11,63 +11,65 @@ router.get("/products", async (req, res) => {
 
 // Agrega un producto/docuento
 router.post('/products', (req, res) => {
-  console.log("Recibiendo datos para insertar")
   const product = productSchema(req.body);
+  console.log("Recibiendo datos para insertar nuevo producto: " + product)
   product
       .save()
       .then(data => res.json(data))
-      .catch(error => res.json({message: error}));
+      .catch(err => res.status(400).json({ message: err}));
 });
 
 // Actualiza informacion de un producto por gtin 
-// TODO: Agregar que tambien filtre por linkType
-router.put('/products/:gtin', (req, res) => {
-  console.log("Sale update")
-    const { gtin } = req.params;
-    const { linkType, uri, language } = req.body;
+router.put('/products/:gtin/:linkTypeOld', (req, res) => {
     
+    const { gtin , linkTypeOld } = req.params;
+    console.log("Actualizando producto con gtin:" + gtin );
     productSchema
-      .updateOne({ gtin: gtin }, {$set: {linkType, uri, language}})
+      .updateOne({ gtin: gtin, linkType: linkTypeOld }, {$set: req.body })
       .then(data => res.json(data))
-      .catch(error => res.json({message: error}))
+      .catch(err => res.status(400).json({ message: err}));
+});
+
+
+// Elimina productos de acuerdo al id de mongo 
+router.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  productSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
 });
 
 // Busca por gtin y params opcionales
 router.get("/:gtin(\\d+)", async (req, res) => {
   const { gtin } = req.params || {};
-  const { linkType } = req.query || {};
+  const { linkType } = req.query || {}; // Ir agreganto todos los parametros que manejamos
   const language = req.headers["accept-language"]?.split(",")[0];
 
-  console.log("Solicitud por el gtin: " + gtin);
-  // Se agrega a la query de busqueda el gtin
-  let query = { gtin, linkType, language };
+  var query = { gtin, linkType, language }
 
-  //FIXME var languageResult   = req.headers['accept-language'];
-  // console.log(languageResult)
+  console.log("Query previa a ejecutar busqueda: " + JSON.stringify(query)); 
 
-  // TODO: Delete. Para pruebas y para ver como queda la query antes del find
-  console.log("Query previa a ejecutar: " + JSON.stringify(query));
-  try {
-    let docs = await productSchema.find(query);
-    if (docs[0]) {
-      // Redireccion al sitio que tiene cargado el registro
-      console.log("Resultado de busqueda find: " + docs);
-      res.status(301).redirect(docs[0].uri);
-      return;
-    }
-    docs = await productSchema.find({ gtin, linkType: "gs1:defaultLink" });
-    console.log("No hubo resutados redireccionando a default");
-    if (docs[0]) {
-      // Redireccion al sitio que tiene cargado el registro
-      console.log("Resultado de busqueda default find: " + docs);
-      res.status(301).redirect(docs[0].uri);
-      return;
-    }
-    res.status(404).json({ message: "Product Not found" });
-  } catch (error) {
+  productSchema.find(query).then(docs =>
+    {
+      if (docs[0]) {
+        console.log("Resultado de busqueda por todos los params: " + docs);
+        res.status(301).redirect(docs[0].uri); // Redireccion al sitio que tiene cargado el registro
+        return;
+      }else {
+        productSchema.find({ gtin, linkType: "gs1:defaultLink" });
+        console.log("No hubo resutados redireccionando a default");
+        if (docs[0]) {
+          console.log("Resultado de la busqueda por default : " + docs);
+          res.status(301).redirect(docs[0].uri); // Redireccion al sitio que tiene cargado el registro
+          return;
+        }
+        res.status(404).json({ message: "Product Not found" }); 
+      }
+  }).catch(error => {
     console.error.bind("Error obtenido al ejecutar la busqueda: ", error);
     res.status(400).json({ message: error });
-  }
+  }) 
 });
 
 module.exports = router;
