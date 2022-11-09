5bdd70a3650cb3947acae9fbfc265d6e85db33c5
tomasabreu <42159630+tomasabreu@users.noreply.github.com>
2022-11-09 18:31:03 -0300

    Fixing tests

--- __tests__/productService.spec.js
+++ __tests__/productService.spec.js
@@ -1,23 +1,18 @@
+const app = require("../server.js");
+const request = require("supertest");
 
-const baseURL = "https://backend-test-resolver.azurewebsites.net"
-//const app = require('../server.js')
-const request = require('supertest')
+jest.setTimeout(30000);
 
-describe('get all products /', () =>{
-  test('should respond 200 status code', async ()=>{
-    const response = await request(baseURL).get('/products').send()
-     expect(response.statusCode).toBe(200)
-     
-  })
-})
+describe("get all products /", () => {
+  test("should respond 200 status code", async () => {
+    const response = await request(app).get("/products");
+    expect(response.statusCode).toBe(200);
+  });
+});
 
-describe('get product with getin 9506000134352',  () => {
-  test('should respond 302 status code', async () => {
-    const response = await request(baseURL).get('/products/9506000134352').send()
-    expect(response.statusCode).toBe(302)
-   
-  })
-})
-
-
-
+describe("get product with getin 9506000134352", () => {
+  test("should respond 200 status code", async () => {
+    const response = await request(app).get("/products/9506000134352");
+    expect(response.statusCode).toBe(200);
+  });
+});
