502e84b752e4373d2b9ef50a91391d004ef797dc
pluchinetti <40801333+pluchinetti@users.noreply.github.com>
2022-11-21 00:15:12 -0300

    Validación de tipo de enlace y opciones de idioma en CyU de enlaces
    - Añadí funciones para validar tipos de enlace y opciones de idioma
    - Modifiqué endpoint de /links para que admita ID de proveedor como string
    - Quité console.logs de prueba

--- route/linkService.js
+++ route/linkService.js
@@ -4,10 +4,6 @@ const languageSchema = require("../models/language");
 const providerSchema = require("../models/provider");
 
 const router = require("../server");
-const { Aggregate } = require("mongoose");
-
-
-
 
 /*
   Endpoint para obtener los tipos de enlace registrados.
@@ -51,39 +47,21 @@ router.get("/linksSearch", (req, res) => {
 
   query = JSON.parse(JSON.stringify(query)); // La transformacion es necesaria para que el find agarre bien la query
 
-  console.log("Query previa a ejecutar busqueda: " + JSON.stringify(query));
-
   linkSchema.find(query, function (err, result) {
     if (err) {
-      console.error("Error al buscar el producto con filtros. Message: " + err);
       return res.status(500).send(err);
     } else if (result) {
-      console.error(
-        "Resultado de busqueda por filtros: " + JSON.stringify(result)
-      );
       return res.status(200).send(result);
     } else {
-      console.error(
-        "La busqueda por filtros no encontro un match, buscando el producto por defecto"
-      );
       return linkSchema.find(
         { gtin, linkType: "gs1:defaultLink" },
         function (err, result) {
           if (err) {
-            console.error(
-              "Error al buscar el producto por defecto. Message: " + err
-            );
             res.status(500).send(err);
           } else if (result) {
-            console.error(
-              "Resultado de busqueda por defecto: " + JSON.stringify(result)
-            );
             res.status(200).send(result);
           } else {
-            console.error(
-              "Las busquedas no encontraron productos con el gtin ingresado"
-            );
-            res.status(404).json({ message: "Product Not found" });
+            res.status(404).json({ message: "Link not found" });
           }
         }
       );
@@ -91,39 +69,6 @@ router.get("/linksSearch", (req, res) => {
   });
 });
 
-/*
-  Endpoint para agregar un enlace.
-*/
-router.post("/links", (req, res) => {
-  const link = linkSchema(req.body);
-  console.log("Recibiendo datos para insertar un nuevo enlace: " + link);
-  link
-    .save()
-    .then((data) => res.json(data))
-    .catch((err) => res.status(400).json({ message: err }));
-});
-
-/*
-  Endpoint para actualizar un enlace.
-*/
-router.put("/links/:id", (req, res) => {
-  const { id } = req.params;
-  linkSchema
-    .updateOne({ _id: id }, { $set: req.body })
-    .then((data) => res.json(data))
-    .catch((err) => res.status(400).json({ message: err }));
-});
-
-/*
-  Endpoint para eliminar un enlace.
-*/
-router.delete("/links/:id", (req, res) => {
-  const { id } = req.params;
-  linkSchema
-    .remove({ _id: id })
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-});
 
 
 /*
@@ -148,21 +93,12 @@ router.get("/links/:gtin(\\d+)", async (req, res) => {
   query = Object.assign({}, query, req.query);
   query = JSON.parse(JSON.stringify(query)); // La transformacion es necesaria para que el find agarre bien la query
 
-  console.log("Query previa a ejecutar busqueda: " + JSON.stringify(query));
-
   linkSchema.findOne(query, function (err, result) {
     if (err) {
-      console.error("Error al buscar el producto con filtros. Message: " + err);
       return res.status(500).send(err);
     } else if (result) {
-      console.error(
-        "Resultado de busqueda por filtros: " + JSON.stringify(result)
-      );
       return res.status(301).redirect(result.uri);
     } else {
-      console.error(
-        "La busqueda por filtros no encontro un match, buscando el producto por defecto"
-      );
       return getDefaultLink(gtin, res);
     }
   });
@@ -173,19 +109,10 @@ function getDefaultLink(gtin, res) {
     { gtin, linkType: "gs1:defaultLink" },
     function (err, result) {
       if (err) {
-        console.error(
-          "Error al buscar el enlace por defecto. Message: " + err
-        );
         res.status(500).send(err);
       } else if (result) {
-        console.error(
-          "Resultado de busqueda por defecto: " + JSON.stringify(result)
-        );
         res.status(301).redirect(result.uri);
       } else {
-        console.error(
-          "No se encontró un enlace por defecto para el GTIN especificado."
-        );
         res.status(404).json({ message: "Link not found." });
       }
     }
@@ -197,7 +124,7 @@ function getDefaultLink(gtin, res) {
   corresponden al proveedor especificado.
 */
 router.get("/links", async (req, res) => {
-  const providerID = Number(req.query.providerID)
+  const providerID = req.query.providerID;
 
   providerSchema.aggregate(
     [
@@ -217,13 +144,9 @@ router.get("/links", async (req, res) => {
     ],
     function (err, result) {
       if (err) {
-        console.error(
-          "Error al buscar el enlace por defecto. Message: " + err
-        );
         res.status(500).send(err);
       }
       else if (result.length) {
-        console.log(result);
         res.status(200).json(result[0].links)
       }
       else {
@@ -233,5 +156,90 @@ router.get("/links", async (req, res) => {
   );
 });
 
+
+
+/*
+  Endpoint para agregar un enlace.
+*/
+router.post("/links", async (req, res) => {
+  // Verifica que el tipo de enlace es válido.
+  const isLinkTypeValid = await checkLinkType(req.body.linkType);
+  if (!isLinkTypeValid) {
+    res.status(400).json({ message: "The specified link type isn't valid." }).send();
+    return;
+  }
+
+  // Si se especifica una opción de idioma, verifica sea una opción válida.
+  if (req.body.acceptLanguage !== undefined) {
+    const isLanguageValid = await checkLanguageOpt(req.body.acceptLanguage);
+    if (!isLanguageValid) {
+      res.status(400).json({ message: "The specified acceptLanguage isn't valid." }).send();
+      return;
+    }
+  }
+
+  const link = linkSchema(req.body);
+
+  link
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+
+/*
+  Endpoint para actualizar un enlace.
+*/
+router.put("/links/:id", async (req, res) => {
+  const { id } = req.params;
+
+  // Verifica que el tipo de enlace es válido.
+  const isLinkTypeValid = await checkLinkType(req.body.linkType);
+  if (!isLinkTypeValid) {
+    res.status(400).json({ message: "The specified link type isn't valid." }).send();
+    return;
+  }
+
+  // Si se especifica una opción de idioma, verifica sea una opción válida.
+  if (req.body.acceptLanguage !== undefined) {
+    const isLanguageValid = await checkLanguageOpt(req.body.acceptLanguage);
+    if (!isLanguageValid) {
+      res.status(400).json({ message: "The specified acceptLanguage isn't valid." }).send();
+      return;
+    }
+  }
+
+  // Intenta realizar la actualización del enlace.
+  linkSchema
+    .updateOne({ _id: id }, { $set: req.body })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json({ message: err }));
+});
+
+/*
+  Endpoint para eliminar un enlace.
+*/
+router.delete("/links/:id", (req, res) => {
+  const { id } = req.params;
+  linkSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+/*
+  Verifica si un tipo de link es válido o no.
+*/
+async function checkLinkType(linkType) {
+  const result = await linkTypeSchema.findById(linkType).exec();
+  return result !== null;
+}
+
+/*
+  Verifica si la opción de idioma es válida o no.
+*/
+async function checkLanguageOpt(languageOpt) {
+  const result = await languageSchema.findById(languageOpt).exec();
+  return result !== null;
+}
 
 module.exports = router;
