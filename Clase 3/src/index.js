a2c12487ec50d11bb03e7ad35ec20cc296852d1b
EL Jose <jose@elcurti.com>
2023-08-14 21:09:07 -0300

    clase3 part2

---
+++ Clase 3/src/index.js
@@ -0,0 +1,9 @@
+function funcionAgregar(){
+    const prueba= `<div class="tarjeta">' 
+    <h3>Ejemplo1</h3>
+    <p> Prueba tarjeta </p>
+</div>`
+
+document.getElementById("col1").innerHTML += prueba;
+
+}
