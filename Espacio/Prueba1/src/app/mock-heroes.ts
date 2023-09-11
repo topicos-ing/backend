84e328c194a58dca59fb0b1fa0c1f53eb58a289e
EL Jose <jose@elcurti.com>
2023-09-11 19:04:46 -0300

    Clase 10

---
+++ Espacio/Prueba1/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Flash' },
+  { id: 14, name: 'Hombre Araña' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Batman' }
+];
